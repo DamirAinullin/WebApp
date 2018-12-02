8532a65038dc1433983c360b6e9a6522c5104af7
damirainullin <damirainullin@outlook.com>
2018-12-02 04:53:31 -0800

    Added initial backend server side

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 def application(environ, start_response):
 
-    data = "\n".join(environ.get('QUERY_STRING').split("&"))
-
     start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
+        ("Content-Type", "text/plain")
     ])
 
-    return [data]
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
 
